2956e01387d152156b442e69b094eb0d96293b77
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-03 22:42:18 +0300

    make heading

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { LandingComponent } from './landing/landing.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [LandingComponent, AboutUsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
