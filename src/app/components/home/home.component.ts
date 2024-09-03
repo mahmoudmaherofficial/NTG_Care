382965a0d86d7d264d2d4a98a49797bf9c557728
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-04 01:37:00 +0300

    services section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LandingComponent } from './landing/landing.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LandingComponent, AboutUsComponent],
+  imports: [LandingComponent, AboutUsComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
