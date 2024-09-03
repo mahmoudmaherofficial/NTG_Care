f87fc2885ea5d46261619b8e3dc014028471cb2a
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-03 14:51:29 +0300

    Adding home component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  menuIcon = document.getElementById('burger');
+}
