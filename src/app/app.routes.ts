fe9c79c55e0585282eb996c2688e38c259af2a21
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-06 01:24:50 +0300

    adding not found section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DoctorsComponent } from './components/doctors/doctors.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'doctors', component: DoctorsComponent },
+  { path: '**', component: NotFoundComponent },
 ];
