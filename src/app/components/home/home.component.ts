08b7d19d1e9c79cf8582e5f7c7d61bb8e6d7ca23
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-06 00:21:42 +0300

    adding gallety section + removing it from nav

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,12 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from "../footer/footer.component";
+import { GalleryComponent } from "./gallery/gallery.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LandingComponent, AboutUsComponent, ServicesComponent, ContactComponent, FooterComponent],
+  imports: [LandingComponent, AboutUsComponent, ServicesComponent, ContactComponent, FooterComponent, GalleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
