8cc2835955c12663e0985bde8480f86b4d127ae1
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-05 23:43:27 +0300

    doctor routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DoctorsComponent } from './components/doctors/doctors.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'doctors', component: DoctorsComponent },
+];
