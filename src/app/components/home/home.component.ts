c57112d7c40cd5fc6b699e22c8d5af49e385d7c8
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-05 22:01:17 +0300

    finalizing home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { LandingComponent } from './landing/landing.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LandingComponent, AboutUsComponent, ServicesComponent],
+  imports: [LandingComponent, AboutUsComponent, ServicesComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
