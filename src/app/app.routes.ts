11f7365381aafc3ac0e2de5ad97921f546044eb2
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-06 16:44:24 +0300

    starting appointments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DoctorsComponent } from './components/doctors/doctors.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AppointmentsComponent } from './components/appointments/appointments.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'doctors', component: DoctorsComponent },
+  { path: 'appointments', component: AppointmentsComponent },
   { path: '**', component: NotFoundComponent },
 ];
