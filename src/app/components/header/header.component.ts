4f7dc6ba01a63583fa6c3209df01c4955fa48c09
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-09 19:20:17 +0300

    mobile menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MobileMenuComponent } from "./mobile-menu/mobile-menu.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MobileMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
---
+++ src/app/components/header/mobile-menu/mobile-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-mobile-menu',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './mobile-menu.component.html',
+  styleUrl: './mobile-menu.component.scss'
+})
+export class MobileMenuComponent {
+
+}
