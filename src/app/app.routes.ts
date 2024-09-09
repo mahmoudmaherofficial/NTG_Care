e3dfbce9566fe5383f427577b487b4033b76448b
mahmoudmaherofficial <mahmoudmaherofficial@gmail.com>
2024-09-09 16:56:17 +0300

    login/register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { DoctorsComponent } from './components/doctors/doctors.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AppointmentsComponent } from './components/appointments/appointments.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
   { path: 'doctors', component: DoctorsComponent },
   { path: 'appointments', component: AppointmentsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: NotFoundComponent },
 ];
